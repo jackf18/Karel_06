80d0709de503b2c7d4ca2f63c2d18b15b14c027a
jackf18 <jackf18@hsln_17581.local>
2014-12-18 14:23:38 +0000

    i did the assignment

--- Lab06.java
+++ Lab06.java
@@ -2,16 +2,65 @@
 *
 * Description of the program goes here  // provide a brief description
 *
-* @author <Your Name here>  // replace <...> with your name
-* @version <date/of/completion> // replace <...> with the date
+* @author Jack Flaherty  
+* @version 11/15/14 
 */
 
 import edu.fcps.karel2.Display;
 import edu.fcps.karel2.Robot;
+import javax.swing.JOptionPane; 
 
 public class Lab06 {
 
      public static void main(String[] args) {
-          // TODO Your code goes here
+     String filename = JOptionPane.showInputDialog("What robot map?");
+     Display.openWorld("maps/" + filename + ".map");     
+     Display.setSpeed(8);
+     Robot jack = new Robot();
+     Robot jack2 = new Robot(1, 2, Display.EAST, Display.INFINITY);
+     Robot jack3 = new Robot(1, 3, Display.EAST, Display.INFINITY);
+     Robot jack4 = new Robot(1, 4, Display.EAST, Display.INFINITY);
+     Robot jack5 = new Robot(1, 5, Display.EAST, Display.INFINITY);
+     Robot jack6 = new Robot(1, 6, Display.EAST, Display.INFINITY); 
+     
+     while(jack.nextToABeeper()) {
+         jack.move();
+     }
+     while(!jack2.nextToABeeper()) {
+         jack2.move();
+     }
+     while(jack3.frontIsClear()) {
+         jack3.move();
+     }
+    
+    if(jack4.nextToABeeper()) {
+             jack4.pickBeeper();
+         }
+     while(jack4.frontIsClear()) {       
+        jack4.move();
+         if(jack4.nextToABeeper()) {
+             jack4.pickBeeper();
+         }
+     } 
+    
+    while(jack5.nextToABeeper()) {
+             jack5.pickBeeper();
+         }
+     while(jack5.frontIsClear()) {       
+        jack5.move();
+         while(jack5.nextToABeeper()) {
+             jack5.pickBeeper();
+         }
+     } 
+
+     while(jack6.nextToABeeper()) {  
+         jack6.move();
+         }
+     jack6.move();
+     while(jack6.nextToABeeper()) {
+         jack6.move();
+         }
+  
+       
      }
 }
